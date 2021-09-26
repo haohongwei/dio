7051a49691f1b144685afbcd333e8b6bf520cb4a
郝宏伟 <haohongwei5@jd.com>
2021-09-26 11:37:55 +0800

    add golableCookie whenever send a request ，use it as request cookie

--- plugins/cookie_manager/lib/src/cookie_mgr.dart
+++ plugins/cookie_manager/lib/src/cookie_mgr.dart
@@ -9,21 +9,24 @@ class CookieManager extends Interceptor {
   /// CookieJar please refer to [cookie_jar](https://github.com/flutterchina/cookie_jar)
   final CookieJar cookieJar;
 
+  List<Cookie> golableCookie = [] ;
+
   CookieManager(this.cookieJar);
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    cookieJar.loadForRequest(options.uri).then((cookies) {
-      var cookie = getCookies(cookies);
+    // cookieJar.loadForRequest(options.uri).then((cookies) {
+      var cookie = getCookies(golableCookie);
+      // var cookie = getCookies(cookies);
       if (cookie.isNotEmpty) {
         options.headers[HttpHeaders.cookieHeader] = cookie;
       }
       handler.next(options);
-    }).catchError((e, stackTrace) {
-      var err = DioError(requestOptions: options, error: e);
-      err.stackTrace = stackTrace;
-      handler.reject(err, true);
-    });
+    // }).catchError((e, stackTrace) {
+    //   var err = DioError(requestOptions: options, error: e);
+    //   err.stackTrace = stackTrace;
+    //   handler.reject(err, true);
+    // });
   }
 
   @override
@@ -57,12 +60,36 @@ class CookieManager extends Interceptor {
 
   Future<void> _saveCookies(Response response) async {
     var cookies = response.headers[HttpHeaders.setCookieHeader];
+    var requestCookies = response.requestOptions.headers[HttpHeaders
+        .cookieHeader];
 
     if (cookies != null) {
-      await cookieJar.saveFromResponse(
-        response.requestOptions.uri,
-        cookies.map((str) => Cookie.fromSetCookieValue(str)).toList(),
-      );
+      var cookiesList = cookies.map((str) => Cookie.fromSetCookieValue(str))
+          .toList();
+      var requestCookiesList = cookies.map((str) =>
+          requestCookies.fromSetCookieValue(str)).toList();
+      for (var cookie in cookiesList) {
+        if (cookie.domain == null) {
+          cookie.domain = response.requestOptions.uri.host;
+        }
+      }
+      for (var cookie in cookiesList) {
+        bool cookieItemUpdated = false;
+        for (var current in requestCookiesList) {
+          if (cookie.name == current.name) {
+            current.value = cookie.value;
+            cookieItemUpdated = true;
+            break;
+          }
+          if(cookieItemUpdated == false){
+            golableCookie.add(cookie);
+          }
+        }
+        await cookieJar.saveFromResponse(
+          response.requestOptions.uri,
+          cookies.map((str) => Cookie.fromSetCookieValue(str)).toList(),
+        );
+      }
     }
   }
 
